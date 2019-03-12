5f64c24880fba7b357562a27ec9193b1afd55b25
Dimitar Vatev <div007@gmail.com>
2019-03-12 18:44:34 +0200

    Conditions and tasks: 01. - 05. - 4-5.Migration 60/100

---
+++ 10. EntityFramework Code-First - Exercise/P03_SalesDatabase.Data/SalesContext.cs
@@ -0,0 +1,144 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class SalesContext : DbContext
+    {
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        // Add follow NuGet Packages:
+        // - Microsoft.EntityFrameworkCore.SqlServer - v 2.2.0
+        // - Microsoft.EntityFrameworkCore.SqlServer.Design - v 1.1.6 - Uninstall for Judge 
+        // - Microsoft.EntityFrameworkCore.Tools - v 2.2.0 - Uninstall for Judge
+        //
+        // Use commands in Package Manager Console:
+        // - Add-Migration {MigratioName}
+        // - Update-Database
+        // to revert
+        // - Update-Database {MigratioName}
+        // - Remove-Migration
+
+        // Judge 4-5 60/100 !!!
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(Config.ConnectionString);
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigureProductEntity(modelBuilder);
+
+            ConfigureCustomerEntity(modelBuilder);
+
+            ConfigureStoreEntity(modelBuilder);
+
+            ConfigureSaleEntity(modelBuilder);
+
+        }
+
+        private void ConfigureSaleEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Sale>()
+                .HasKey(s => s.SaleId);
+
+            modelBuilder
+                .Entity<Sale>()
+                .HasOne(s => s.Store)
+                .WithMany(s => s.Sales);
+
+            modelBuilder
+                .Entity<Sale>()
+                .HasOne(s => s.Customer)
+                .WithMany(c => c.Sales);
+
+            modelBuilder
+                .Entity<Sale>()
+                .HasOne(s => s.Product)
+                .WithMany(p => p.Sales);
+
+            modelBuilder
+                .Entity<Sale>()
+                .Property(s => s.Date)
+                .HasDefaultValueSql("GETDATE()");
+        }
+
+        private void ConfigureStoreEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Store>()
+                .HasKey(s => s.StoreId);
+
+            modelBuilder
+                .Entity<Store>()
+                .Property(s => s.Name)
+                .HasMaxLength(80)
+                .IsUnicode()
+                .IsRequired();
+        }
+
+        private void ConfigureCustomerEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Customer>()
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Name)
+                .HasMaxLength(100)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.Email)
+                .HasMaxLength(80)
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Customer>()
+                .Property(c => c.CreditCardNumber)
+                .HasMaxLength(20)
+                .IsRequired();
+        }
+
+        private void ConfigureProductEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Product>()
+                .HasKey(p => p.ProductId);
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(p => p.Name)
+                .HasMaxLength(50)
+                .IsUnicode()
+                .IsRequired();
+
+            modelBuilder
+                .Entity<Product>()
+                .Property(p => p.Description)
+                .HasMaxLength(250)
+                .IsUnicode();
+
+            //modelBuilder
+            //    .Entity<Product>()
+            //    .Property(p => p.Quantity)
+            //    .HasColumnType("decimal(18, 3)")
+            //    .IsRequired();
+
+            //modelBuilder
+            //    .Entity<Product>()
+            //    .Property(p => p.Price)
+            //    .HasColumnType("decimal(18, 2)")
+            //    .IsRequired();
+        }
+    }
+}
---
+++ 10. EntityFramework Code-First - Exercise/P03_SalesDatabase/StarUp.cs
@@ -0,0 +1,62 @@
+﻿namespace P03_SalesDatabase
+{
+    using System;
+    using System.Collections.Generic;
+    using Data;
+    using Data.Models;
+
+    public class StarUp
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SalesContext())
+            {
+                var productsToAdd = GetProductsToSeed();
+
+                db.Products.AddRange(productsToAdd);
+
+                db.SaveChanges();
+
+                var store = new Store
+                {
+                    Name = "Lidl"
+                };
+
+                db.Stores.Add(store);
+                db.SaveChanges();
+            }
+        }
+
+        private static List<Product> GetProductsToSeed()
+        {
+            List<Product> products = new List<Product>();
+
+            var product1 = new Product
+            {
+                Name = "Banana",
+                Price = 2.50M,
+                Quantity = 10
+            };
+
+            var product2 = new Product
+            {
+                Name = "Tomato",
+                Price = 4M,
+                Quantity = 20
+            };
+
+            var product3 = new Product
+            {
+                Name = "Apple",
+                Price = 1.50M,
+                Quantity = 15
+            };
+
+            products.Add(product1);
+            products.Add(product2);
+            products.Add(product3);
+
+            return products;
+        }
+    }
+}
